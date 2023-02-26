824d129a06e92a545c3632b9e2c23984aec8c375
Deborah How <hyh.deb@gmail.com>
2023-02-26 21:10:02 +0800

    added InputFunction.java to test it out

--- backend/src/main/java/com/quantum/backend/model/Form.java
+++
@@ -1,7 +0,0 @@
-package com.quantum.backend.model;
-
-public interface Form {
-    public void getCompanyName();
-    public void getLastSubmittedDate();
-    public void getFormName();
-}
---
+++ backend/src/main/java/com/quantum/backend/model/VendorAssessmentForm.java
@@ -0,0 +1,13 @@
+package com.quantum.backend.model;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class VendorAssessmentForm {
+
+    private InputFunction countryName;
+    private InputFunction companyName;
+    private InputFunction representativeEmail;
+    
+}
