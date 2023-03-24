09384002faaaabc8203238abc5264ca3faadcce0
Jeral Ong <jerrysmalleyes@gmail.com>
2023-03-24 19:01:40 +0800

    Update for create workflow

--- frontend/src/views/CreateWorkflow.vue
+++ frontend/src/views/CreateWorkflow.vue
@@ -20,13 +20,21 @@
             <td>
               <input type="text" id="workflowName" v-model="workflowName" style="width: 80%" />
             </td>
+          </tr>
+          <tr>
+            <td>
+              <label for="workflowName">Deadline</label>
+            </td>
+            <td>
+              <input type="text" id="deadline" v-model="deadline" style="width: 80%" />
+            </td>
           </tr>         
           <tr>
             <td>
-              <label for="forms">Select Form:</label>
+              <label for="forms">Select Forms:</label>
             </td>
             <td>
-              <select v-model="selectedForms" id="form" style="width: 80%" multiple>
+              <select v-model="selectedForms" id="form" style="width: 50%" multiple>
                 <option v-for="form in this.allForms" :key="form.formId" :value="form">
                   {{ form.formName }}
                 </option>
@@ -35,12 +43,54 @@
           </tr>
           <tr>
             <td>
-              <label for="users">Select Users:</label>
+              <label for="users">Select Assigned Users:</label>
             </td>
             <td>
-              <select v-model="selectedUsers" id="user" style="width: 80%" multiple>
+              <select v-model="selectedUsers" id="user" style="width: 50%" multiple>
                 <option v-for="user in this.allUsers" :key="user.userId" :value="user">
                   {{ user.username }}
+                </option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label for="forms">Select Assigned Vendors:</label>
+            </td>
+            <td>
+              <select
+                v-model="selectedVendors"
+                id="vendorSelect"
+                style="width: 50%"
+                multiple
+              >
+                <option
+                  v-for="vendor in this.allVendors"
+                  :key="vendor.userId"
+                  :value="vendor"
+                >
+                  {{ vendor.username }}
+                </option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label for="forms">Select Assigned Admins:</label>
+            </td>
+            <td>
+              <select
+                v-model="selectedAdmins"
+                id="adminSelect"
+                style="width: 50%"
+                multiple
+              >
+                <option
+                  v-for="admin in this.allAdmins"
+                  :key="admin.userId"
+                  :value="admin"
+                >
+                  {{ admin.username }}
                 </option>
               </select>
             </td>
@@ -67,10 +117,15 @@
       return {
         userType: localStorage.userType,
         workflowName: "",
+        deadline: "",
         allForms: [],
-        selectedForms: [],
+        selectedForms:{},
         allUsers:[],
-        selectedUsers: []
+        selectedUsers: [],
+        allAdmins: [],
+        allVendors: [],
+        selectedAdmins: [],
+        selectedVendors: []
       };
     },
     methods: {
@@ -85,38 +140,74 @@
         this.workflowName = "";
         this.form = "";
       },
-      createWorkflow() {
+      createWorkflow: function () {
         if (this.userType === "ROLE_ADMIN" || this.userType === "ROLE_APPROVER") {
-          const data = {
-            workflowName: this.workflowName,
-            form: this.form,
-            assignedUsers: [
-              {
-                userId: "641468eaa526a31d0609d563",
-                userType: "ROLE_ADMIN",
-                username: "jasmine88888",
-                password:
-                  "$2a$10$/HSxHv7n1KPCNy2N6r1Ly.YkvZwNygY1EFcuZxnf4uK4NXal6FeUq",
-                email: "jasmine-lim88@hotmail.com",
-              },
-            ],
-          };
-
-          axios
-            .post("http://localhost:8080/api/workflow/create", data, {
+        axios
+          .post(
+            "http://localhost:8080/api/workflow/create",
+            this.data,
+            {
               headers: {
-                "Content-Type": "application/json",
-                Authorization: "Bearer " + localStorage.token,
-                "Access-Control-Allow-Origin": "*",
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + localStorage.token,
+              "Access-Control-Allow-Origin": "*",
               },
-            })
-            .then((response) => {
-              console.log(response);
-              this.$router.push({ name: "ViewWorkflow" });
-            })
-            .catch((error) => {
-              alert("Error:" + error.response.status);
-            });
+            }
+          )
+          .then((response) => {
+            console.log(response);
+            this.$router.push({ name: "ViewWorkflow" });
+          })
+          .catch(function (error) {
+            alert("Error:" + error.response.status);
+          });
+        }
+      },
+      resetWorkflow: function () {
+        this.data = {};
+      },
+      addStep: function () {
+        this.data.steps.push({
+          name: "",
+          description: "",
+          assignedUsers: [],
+          form: {},
+        });
+      },
+      removeStep: function (index) {
+        this.data.steps.splice(index, 1);
+      },
+      addForm: function () {
+        this.allForms.push({
+          formId: "",
+          formName: "",
+        });
+      },
+      removeForm: function (index) {
+        this.allForms.splice(index, 1);
+      },
+      cancel: function () {
+        this.$router.push({ name: "ViewWorkflow" });
+      },
+      validateStep: function (step) {
+        if (step.name === "" || step.description === "" || step.assignedUsers.length === 0 || !step.form.formId) {
+          return false;
+        }
+        return true;
+      },
+      validateWorkflow: function () {
+        for (let i = 0; i < this.data.steps.length; i++) {
+          if (!this.validateStep(this.data.steps[i])) {
+          return false;
+          }
+        }
+        return true;
+      },
+      createWorkflowSubmit: function () {
+        if (this.validateWorkflow()) {
+          this.createWorkflow();
+        } else {
+          alert("Please fill all the fields for each step and select a form.");
         }
       },
       getAllUsers() {
@@ -129,9 +220,17 @@
             },
           })
           .then((response) => {
-            this.allUsers = response.data;
-            console.log(this.allUsers);
-          });
+          this.allUsers = response.data;
+          console.log(this.allUsers);
+          for (let user of this.allUsers) {
+            if (user.userType == "ROLE_ADMIN") {
+              this.allAdmins.push(user);
+            }
+            if (user.userType == "ROLE_VENDOR") {
+              this.allVendors.push(user);
+            }
+          }
+        });
       },
       getAllForms() {
         axios
@@ -152,7 +251,9 @@
       this.getAllUsers();
       this.getAllForms();
       this.selectedUsers = this.data.assignedUsers;
-      this.selectedForm = this.data.form
+      this.selectedForm = this.data.form;
+      this.selectedAdmins = this.data.assignedAdmins;
+      this.selectedVendors = this.data.assignedVendors;
       console.log(" on create workflow page");
     } catch (e) {
       if (e instanceof SyntaxError) {
